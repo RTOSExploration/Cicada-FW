b2343d0ff419a5eb185b5d4f89e459b56c1df47e
Mingjie Shen <shen497@purdue.edu>
2023-04-29 18:54:36 +0000

    update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-DB=$RTOSExploration/bitcode-db/Cicada-FW
-rm -rf $DB build && mkdir -p build
+#DB=$RTOSExploration/bitcode-db/Cicada-FW
+#rm -rf $DB build && mkdir -p build
+#meson build --cross-file stm32.cross.build
+
+# To setup build dependencies, do: git submodule init git submodule update
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export BINUTILS_TARGET_PREFIX=$TOOLCHAIN_BIN/arm-none-eabi
+export CC=wllvm
+rm -rf build && mkdir -p build
 meson build --cross-file stm32.cross.build
-cd build && genbc $DB -c "ninja"
+cd build && ninja
